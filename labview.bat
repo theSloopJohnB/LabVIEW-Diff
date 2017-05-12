32a1d7609256d7b8d683e6bd29a331a78980acbd
theSloopJohnB <john.boyd@ni.com>
2017-05-12 14:04:09 -0500

    git diff tools added
    Added a translation layer from the git difftool to the labview stuff I
    Want to run

---
+++ labview.bat
@@ -0,0 +1,4 @@
+
+SET vi1=%~f1
+SET vi2=%~f2
+"C:\Program Files (x86)\National Instruments\LabVIEW 2014\LabVIEW.exe" C:\Users\nitest\Documents\GitHub\LabVIEW-Diff\Main.vi -- "%vi1%" "%vi2%" C:\Users\nitest\temp
