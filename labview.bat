bec1d107971d8c87d5b007cdcc7357b8d4049496
theSloopJohnB <john.boyd@ni.com>
2017-05-12 14:43:24 -0500

    save vis
    Adding saves to the VIs to make sure we dont get prompted to save on
    file close.

--- labview.bat
+++ labview.bat
@@ -1,4 +1,4 @@
-
+echo Please be reading this file!
 SET vi1=%~f1
 SET vi2=%~f2
 "C:\Program Files (x86)\National Instruments\LabVIEW 2014\LabVIEW.exe" C:\Users\nitest\Documents\GitHub\LabVIEW-Diff\Main.vi -- "%vi1%" "%vi2%" C:\Users\nitest\temp
