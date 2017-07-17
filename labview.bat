271e66f004ef3dbc6fdd0ecd4ab3edbfe8bcbff3
theSloopJohnB <john.boyd@ni.com>
2017-07-17 11:23:34 -0500

    only running labview if VI is found. (e.g. it's not new)

--- labview.bat
+++ labview.bat
@@ -1,4 +1,14 @@
-echo Please be reading this file!
 SET vi1=%~f1
 SET vi2=%~f2
-"C:\Program Files (x86)\National Instruments\LabVIEW 2014\LabVIEW.exe" C:\Users\nitest\Documents\GitHub\LabVIEW-Diff\Main.vi -- "%vi1%" "%vi2%" C:\Users\nitest\temp
+
+REM only run if the file is a vi.
+
+if "%vi1:~-3%" == ".vi" GOTO :DIFF_VI
+
+GOTO :END
+
+
+:DIFF_VI
+    "C:\Program Files (x86)\National Instruments\LabVIEW 2014\LabVIEW.exe" C:\Users\nitest\Documents\GitHub\LabVIEW-Diff\Main.vi -- "%vi1%" "%vi2%" C:\Users\nitest\temp
+
+:END
